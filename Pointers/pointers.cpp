def1861d130f9ed8337035acf69611d8d4cda5c0
Anand Sharma <shringianandsharma@gmail.com>
2023-11-21 21:44:09 +0530

    day 4

--- Pointers/pointers.cpp
+++ Pointers/pointers.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <stdio.h>
-
+#include <stdlib.h>
 using namespace std;
 
 int main(){
-
+   
+   cout<<"//Part 1/////////////////////////////////////////////////////\n"<<endl;
  int a = 10;
 
  int *p;
@@ -17,8 +18,7 @@ int main(){
 
  cout<<"value in pointer "<< p <<endl;
 
-
-
+cout<<"\n//Part 2/////////////////////////////////////////////////////\n"<<endl;
  int arr[] = {2,4,6,8,10};
 
  int *s;
@@ -26,6 +26,26 @@ int main(){
 
  for(int i=0; i<5; i++){
     cout<<s[i]<<endl;}
-    
+
+
+
+ cout<<"\n//Part 3/////////////////////////////////////////////////////\n"<<endl;
+
+   int *p1;
+
+   // p= (int *)malloc(5 * sizeof(int));
+   //c method
+
+   // c++ method 
+
+   p1 = new int[5];
+
+   p1[0]=10; p1[1]= 20; p1[2]= 30; p1[3]= 31; p1[4]= 44;
+
+
+   for(int i=0; i<5; i++){
+    cout<<p1[i]<<endl;}
+
+
  return 0;
 }
